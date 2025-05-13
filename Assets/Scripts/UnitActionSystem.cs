8f48f417aa98a04b3063e6270bd525eea64b0ec0
Berkay Kiziltug <berkay.kzt@gmail.com>
2025-05-13 19:28:38 +0300

    add selected unit logic and visual

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,19 +1,33 @@
+using System;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
-    
+    public static UnitActionSystem Instance { get; private set; }
+    public event EventHandler OnSelectedUnitChanged;
    [SerializeField] private Unit selectedUnit;
    [SerializeField] private LayerMask unitLayerMask;
 
-    void Update()
+   private void Awake()
+   {
+       if (Instance != null && Instance != this)
+       {
+           Destroy(this.gameObject);
+       }
+       else
+       {
+           Instance = this;
+       }
+   }
+
+   void Update()
     {
          if (Input.GetMouseButtonDown(0))
-        { 
-            //check if you can select the unit. If yes then just return until next mouse click so we don't have the character move instantly after selecting the unit.
-            if(TryHandleUnitSelection()) return;
-           selectedUnit.Move(MouseWorld.GetMouseWorldPosition());
-        }
+         { 
+             //check if you can select the unit. If yes then just return until next mouse click so we don't have the character move instantly after selecting the unit.
+             if(TryHandleUnitSelection()) return;
+             selectedUnit.Move(MouseWorld.GetMouseWorldPosition());
+         }
     }
 
 
@@ -23,10 +37,22 @@ public class UnitActionSystem : MonoBehaviour
         if(Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask)){ 
             if(raycastHit.transform.TryGetComponent<Unit>(out Unit unitComponent))
             {
-                selectedUnit = unitComponent;
+                //Invoking the event inside this method.
+                SetSelectedUnit(unitComponent);
                 return true;
             }
         }
         return false;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
