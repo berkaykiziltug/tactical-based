e343cf66951fc6875a6de2d74801f4beff7da788
Berkay Kiziltug <berkay.kzt@gmail.com>
2025-05-13 15:23:12 +0300

    set up animations

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+
+    [SerializeField]private Animator unitAnimator;
     private Vector3 targetPosition;
     
     private void Update()
     {
+        
         float stopDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stopDistance)
         {
+            unitAnimator.SetBool("isWalking", true);
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             // movement is not dependent on framerate since we multiply with Time.deltaTime
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }else{
+            unitAnimator.SetBool("isWalking",false);
         }
 
         if (Input.GetMouseButtonDown(0))
