6d81e4552adc41153731abc8a57de744607c039f
berkaykiziltug <berkay.kzt@gmail.com>
2025-06-21 10:43:38 +0300

    add door and interaction system

---
+++ Assets/Scripts/Action/InteractAction.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractAction : BaseAction
+{
+    private int maxInteractDistance = 1;
+
+    private void Update()
+    {
+        if (!isActive) return;
+        
+    }
+
+    public override string GetActionName()
+    {
+        return  "Interact";
+    }
+
+    public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
+    {
+        Door door = LevelGrid.Instance.GetDoorAtGridPosition(gridPosition);
+        door.Interact(OnInteractComplete);
+        ActionStart(onActionComplete);
+    }
+
+    private void OnInteractComplete()
+    {
+        ActionComplete();
+    }
+
+    public override List<GridPosition> GetValidActionGridPositionList()
+    {
+        List<GridPosition> validGridPositionList = new List<GridPosition>();
+       
+        GridPosition unitGridPosition = unit.GetGridPosition();
+
+        // GridPosition unitGridPosition = unit.GetGridPosition();
+        for (int x = -maxInteractDistance; x <= maxInteractDistance; x++)
+        {
+            for (int z = -maxInteractDistance; z <= maxInteractDistance; z++)
+            {
+                //Cycle through all the grid positions in maxMoveDistance range. Starting from - to + (left to right and bottom to top) and make grid positions
+                //I can get whatever data I want on the cycled positions. Then add it to current Unit's position and store it inside test grid position.
+                GridPosition offsetGridPosition = new GridPosition(x, z);
+                GridPosition testGridPosition = unitGridPosition + offsetGridPosition;
+                if (!LevelGrid.Instance.IsValidGridPosition(testGridPosition))
+                {
+                    continue;
+                }
+                Door door = LevelGrid.Instance.GetDoorAtGridPosition(testGridPosition);
+                if (door == null)
+                {
+                    //No door on this gridPosition
+                    continue;
+                }
+                
+                validGridPositionList.Add(testGridPosition);
+            }
+        }
+        return validGridPositionList;
+    }
+
+    public override EnemyAIAction GetEnemyAIAction(GridPosition gridPosition)
+    {
+        return new EnemyAIAction
+        {
+            gridPosition = gridPosition,
+            actionValue = 0
+        };
+    }
+}
--- Assets/Scripts/Grid/LevelGrid.cs
+++ Assets/Scripts/Grid/LevelGrid.cs
@@ -6,12 +6,13 @@ public class LevelGrid : MonoBehaviour
 {
     public static LevelGrid Instance;
 
-   [SerializeField] private int width;
-   [SerializeField] private int height;
-   [SerializeField] private float cellSize;
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+    [SerializeField] private float cellSize;
     public event EventHandler OnAnyUnitMovedGridPosition;
     [SerializeField] private Transform debugPrefab;
     private GridSystem<GridObject> gridSystem;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -22,7 +23,9 @@ public class LevelGrid : MonoBehaviour
         {
             Instance = this;
         }
-        gridSystem = new GridSystem<GridObject>(width, height,cellSize, (GridSystem<GridObject> g,GridPosition gridPosition) => new GridObject(g,gridPosition));
+
+        gridSystem = new GridSystem<GridObject>(width, height, cellSize,
+            (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
         //gridSystem.CreateDebugObjects(debugPrefab);
     }
 
@@ -55,23 +58,35 @@ public class LevelGrid : MonoBehaviour
         AddUnitAtGridPosition(toGridPosition, unit);
         OnAnyUnitMovedGridPosition?.Invoke(this, EventArgs.Empty);
     }
-    
+
     public GridPosition GetGridPosition(Vector3 worldPosition) => gridSystem.GetGridPosition(worldPosition);
     public Vector3 GetWorldPosition(GridPosition gridPosition) => gridSystem.GetWorldPosition(gridPosition);
-    
+
     public bool IsValidGridPosition(GridPosition gridPosition) => gridSystem.IsValidGridPosition(gridPosition);
-    public int  GetWidth() => gridSystem.GetWidth();
-    public int  GetHeight() => gridSystem.GetHeight();
+    public int GetWidth() => gridSystem.GetWidth();
+    public int GetHeight() => gridSystem.GetHeight();
 
     public bool HasAnyUnitOnGridPosition(GridPosition gridPosition)
     {
         GridObject gridObject = gridSystem.GetGridObject(gridPosition);
         return gridObject.HasAnyUnit();
     }
+
     public Unit GetUnitAtGridPosition(GridPosition gridPosition)
     {
         GridObject gridObject = gridSystem.GetGridObject(gridPosition);
         return gridObject.GetUnit();
     }
-    
+
+    public Door GetDoorAtGridPosition(GridPosition gridPosition)
+    {
+        GridObject gridObject = gridSystem.GetGridObject(gridPosition);
+        return gridObject.GetDoor();
+    }
+    public void SetDoorAtGridPosition(GridPosition gridPosition, Door door)
+    {
+        GridObject gridObject = gridSystem.GetGridObject(gridPosition);
+        gridObject.SetDoor(door);
+    }
+
 }
